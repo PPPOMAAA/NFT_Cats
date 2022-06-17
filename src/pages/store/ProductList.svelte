3a49cbe27796e59eef762088b0b62c33d3887f43
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-06-17 19:57:04 +1000

    add fetch for product list

--- src/pages/store/ProductList.svelte
+++ src/pages/store/ProductList.svelte
@@ -1,42 +1,24 @@
 <script>
+import { onMount } from "svelte/internal";
+
     import ProductCard from "./ProductCard.svelte";
 
     export let pageNumber = 0;
 
-    let products = [
-        { id: 0, productName: "BEER", productImage: "", authorName: "Pedro", price: 75.7},
-        { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
-        { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
-        { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
-        { id: 4, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 5, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 6, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 7, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
-        { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
-        { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
-        { id: 4, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 5, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 6, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},  
-        { id: 7, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
-        { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
-        { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
-        { id: 4, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 5, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 6, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 7, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
-        { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
-        { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
-        { id: 4, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 5, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 6, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 7, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
-        { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
-        { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
-        { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
-    ];
+    let products = [];
+
+    onMount(async () => {
+        const res = await fetch("store/page=" + pageNumber);
+        products = await res.json();
+    });
+
+    // let products = [
+    //     { id: 0, productName: "BEER", productImage: "", authorName: "Pedro", price: 75.7},
+    //     { id: 1, productName: "VODKA", productImage: "", authorName: "Pedro", price: 34.2},
+    //     { id: 2, productName: "GAYBOY", productImage: "", authorName: "Roma", price: 332.99},
+    //     { id: 3, productName: "ILIYA", productImage: "", authorName: "Roma", price: 0.02},
+    //     { id: 4, productName: "ABODBUHOV", productImage: "", authorName: "Roma", price: 0.13},
+    // ];
 </script>
 
 
